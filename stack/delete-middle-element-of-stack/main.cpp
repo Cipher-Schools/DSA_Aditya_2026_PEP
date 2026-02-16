7f861aa5dca70206ea7eadd27a9c80a9ea5654a5
Aditya Saxena <adity09@outlook.com>
2026-02-16 19:52:17 +0530

    Add stack implementations and solutions
    This commit introduces implementations and solutions for several
    stack-related problems:
    
    - **Delete Middle Element of a Stack**: A solution to delete the middle
      element of a given stack.
    - **Implement Stack Using Queues**: A class that implements a stack
      using only queue operations.
    - **Next Greater Element II**: Finds the next greater element for each
      element in a circular array.
    - **Next Greater Element I**: Finds the next greater element for
      elements of `nums1` that are present in `nums2`.
    - **Valid Parentheses**: Checks if a string containing only '(', ')',
      '{', '}', '[' and ']' is valid.

---
+++ stack/delete-middle-element-of-stack/main.cpp
@@ -0,0 +1,23 @@
+// https://www.geeksforgeeks.org/problems/delete-middle-element-of-a-stack/1
+
+class Solution {
+public:
+  int sz;
+
+  void delMid(stack<int> &st, int ind) {
+    if (ind == (sz + 1) / 2) {
+      st.pop();
+      return;
+    }
+
+    int top = st.top();
+    st.pop();
+    delMid(st, ind - 1);
+    st.push(top);
+  }
+
+  void deleteMid(stack<int> &s) {
+    sz = s.size();
+    delMid(s, sz);
+  }
+};
---
+++ stack/next-greater-element-ii/main.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/next-greater-element-ii/description/
+
+class Solution {
+public:
+  vector<int> nextGreaterElements(vector<int> &nums) {
+    int sz = nums.size();
+    vector<int> ans(sz, -1);
+    stack<int> st;
+
+    for (int i = 0; i < 2 * sz; ++i) {
+      while (!st.empty() && nums[i % sz] > nums[st.top()]) {
+        ans[st.top()] = nums[i % sz];
+        st.pop();
+      }
+      st.push(i % sz);
+    }
+
+    return ans;
+  }
+};
